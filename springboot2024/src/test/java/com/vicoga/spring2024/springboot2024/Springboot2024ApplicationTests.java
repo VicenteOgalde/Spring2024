00f5c4fcd7d4f9f3d729cb2a35f2c838aa905635
Vicente Ogalde <vicenteogalde@gmail.com>
2024-02-03 18:19:05 +1300

    commit 1

---
+++ springboot2024/src/test/java/com/vicoga/spring2024/springboot2024/Springboot2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicoga.spring2024.springboot2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
