1727935d98bc4364a6c996f1797d0e779feb7122
Vicente Ogalde <vicenteogalde@gmail.com>
2024-02-04 20:50:32 +1300

    commit 5

--- springboot2024/src/main/java/com/vicoga/spring2024/springboot2024/controllers/UserRestController.java
+++ springboot2024/src/main/java/com/vicoga/spring2024/springboot2024/controllers/UserRestController.java
@@ -5,9 +5,10 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-
+@RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping("/details2")
