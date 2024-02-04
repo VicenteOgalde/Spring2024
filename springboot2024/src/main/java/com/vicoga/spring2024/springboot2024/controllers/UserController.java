4de56d229a8d08a17504f21dc05c791ab4b2ba07
Vicente Ogalde <vicenteogalde@gmail.com>
2024-02-05 11:54:19 +1300

    commit 6.0

--- springboot2024/src/main/java/com/vicoga/spring2024/springboot2024/controllers/UserController.java
+++ springboot2024/src/main/java/com/vicoga/spring2024/springboot2024/controllers/UserController.java
@@ -4,13 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.vicoga.spring2024.springboot2024.models.User;
+
 @Controller
 public class UserController {
 
     @GetMapping("/details")
     public String details(Model model){
+        User user = new User("vicente", "ogalde");
+
         model.addAttribute("title", "Hello spring 2024");
-        model.addAttribute("name", "vicente");
+        model.addAttribute("user", user);
         return "details";
     }
 
--- springboot2024/src/main/java/com/vicoga/spring2024/springboot2024/controllers/UserRestController.java
+++ springboot2024/src/main/java/com/vicoga/spring2024/springboot2024/controllers/UserRestController.java
@@ -5,6 +5,9 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vicoga.spring2024.springboot2024.models.User;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -13,9 +16,11 @@ public class UserRestController {
 
     @GetMapping("/details2")
     public Map<String,Object> details(){
+
+        User user = new User("vicente", "ogalde");
         Map<String,Object> model= new HashMap<>();
         model.put("title", "Hello spring 2024");
-        model.put("name", "vicente");
+        model.put("user", user);
         return model;
     }
 
