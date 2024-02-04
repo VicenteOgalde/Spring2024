f755af68d93e61d5de8dc2298d7c3c205e33af64
Vicente Ogalde <vicenteogalde@gmail.com>
2024-02-04 20:24:26 +1300

    commit 3

--- springboot2024/src/main/java/com/vicoga/spring2024/springboot2024/controllers/UserController.java
+++ springboot2024/src/main/java/com/vicoga/spring2024/springboot2024/controllers/UserController.java
@@ -1,14 +1,16 @@
 package com.vicoga.spring2024.springboot2024.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
-
+    public String details(Model model){
+        model.addAttribute("title", "Hello spring 2024");
+        model.addAttribute("name", "vicente");
         return "details";
     }
 
