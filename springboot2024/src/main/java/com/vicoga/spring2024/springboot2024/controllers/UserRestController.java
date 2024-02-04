59cfb985fe33ddcd98c4ce74c7d1f38c492cab8f
Vicente Ogalde <vicenteogalde@gmail.com>
2024-02-04 20:44:09 +1300

    commit 4

---
+++ springboot2024/src/main/java/com/vicoga/spring2024/springboot2024/controllers/UserRestController.java
@@ -0,0 +1,21 @@
+package com.vicoga.spring2024.springboot2024.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserRestController {
+
+    @GetMapping("/details2")
+    public Map<String,Object> details(){
+        Map<String,Object> model= new HashMap<>();
+        model.put("title", "Hello spring 2024");
+        model.put("name", "vicente");
+        return model;
+    }
+
+}
